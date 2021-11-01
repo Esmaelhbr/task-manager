2dc1b3c4fc34a012b143739a37c76277e3c3941f
Esmaelhbr <esmael7@live.com>
2021-11-01 17:57:07 -0400

    update

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,16 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionUrl ="mongodb://127.0.0.1:27017";
 const databaseName ="task-manager";
+
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionUrl, {useNewUrlParser: true}, 
 	               (error, client) => {
@@ -10,9 +18,69 @@ MongoClient.connect(connectionUrl, {useNewUrlParser: true},
 		return console.log("Unable to connect to database");
 	}
 	const db = client.db(databaseName);
+
+
+
+	//update 
+	// db.collection('users').updateOne({
+ //        _id:ObjectID("617cb2c5e6302026bc41dd33")
+	// }, {
+	// 	$inc:{
+	// 		age: 8
+	// 	}
+	// }).then((resut)=> {
+	// 	console.log(result);
+
+	// }).catch((error)=> {
+	// 	console.log(error)
+
+	// })
+
+
+	db.collection('tasks').updateMany({
+		status: false
+	},{
+		$set : {
+			status: true
+		}
+		
+	}).then((result) => {
+		console.log(result)
+	}).catch((error) => {
+		console.log(error)
+	})
+
+
+
+	// db.collection('users').findOne({_id: new ObjectID("617cc53378db6723508f6965")}, (error, user) => {
+	// 	if(error){
+	// 		return console.log('unable to feach')
+	// 	}
+	// 	console.log(user);
+	// })
+
+	// db.collection('users').find({age:13}).toArray((error, users) => {
+	// 	console.log(users)
+	// });
+	// db.collection('users').find({age:13}).count((error, count) => {
+	// 	console.log(count)
+	// });
+
+	// db.collection('tasks').findOne(
+	// 	{
+	// 		_id:new ObjectID("617d692a9266f1ee8ce31f68")
+	// 	},(error,user) => {
+	// 		if(error){
+	// 			return console.log("error")
+	// 		}
+	// 		console.log(user)
+
+	// 	})
+
+
 	// db.collection("users").insertOne({
-	// 	name :"James",
-	// 	age: "13"
+	// 	name :"Vikram",
+	// 	age: "40"
 	// }, (error, result) => {
 	// 	if(error){
 	// 		return console.log("unable to insert user");
@@ -41,31 +109,33 @@ MongoClient.connect(connectionUrl, {useNewUrlParser: true},
 
 	// 	})
 
-	db.collection("tasks").insertMany(
-		[
-			{
-				description: "play soccer",
-				status: false
+	// db.collection("tasks").insertMany(
+	// 	[
+	// 		{
+	// 			description: "play soccer",
+	// 			status: false
 
-			},
-			{
-				description: "read a book",
-				status: false
+	// 		},
+	// 		{
+	// 			description: "read a book",
+	// 			status: false
 
-			},
-			{
-				description: "play guitar",
-				status: true
+	// 		},
+	// 		{
+	// 			description: "play guitar",
+	// 			status: true
 
-			}
+	// 		}
 
-		], (error, result) => {
-			if(error){
-				return console.log(error);
-			}else{
-			console.log(result.ops)
-			}
-		})
+	// 	], (error, result) => {
+	// 		if(error){
+	// 			return console.log(error);
+	// 		}else{
+	// 		console.log(result.ops)
+	// 		}
+	// 	})
+
+
 
 
 })
