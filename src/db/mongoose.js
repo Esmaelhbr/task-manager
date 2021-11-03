5ad962148a193a66ec9f44e1c4de5f9d328a7e69
Esmaelhbr <esmael7@live.com>
2021-11-03 12:47:08 -0400

    user and task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 	useNewUrlParser: true,
@@ -6,23 +7,85 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 })
 
 
-const User = mongoose.model('User',
-	{
-		name: {
-			type: String
-		},
-		age: {
-			type: Number
-		}
-	})
+// const User = mongoose.model('User',
+// 	{
+// 		name: {
+// 			type: String,
+// 			required: true,
+// 			trim: true
+// 		},
+// 		email:{
+// 			type: String,
+// 			required: true,
+// 			trim: true,
+// 			lowercase: true,
+// 			validate(value){
+// 				if(!validator.isEmail(value)){
+// 					throw new Error('email is not valid')
+// 				}
+// 			}
+// 		},
+// 		password:{
+// 			type: String,
+// 			required: true,
+// 			trim: true,
+// 			minlength: 7,
+// 			validate(value){
+// 				if( value.includes("password")){
+// 					throw new Error("Invalid password cannot contain password")
+// 				}
 
-const me = new User({
-	name: "Esmael",
-	age: 20
+// 			}
+// 		},
+// 		age: {
+// 			type: Number,
+// 			default: 0,
+// 			validate(value){
+// 				if(value <0 ){
+// 					throw new Error("Age must be a positive number")
+// 				}
+// 			}
+// 		}
+// 	})
+
+// const me = new User({
+// 	name: "  Jackson  ",
+// 	email: 'JohnnyB@hotmail.com    ',
+// 	password: "ease"
+// })
+
+// me.save().then(()=> {
+// 	console.log(me)
+// }).catch((error) =>{
+// 	console.log(error);
+// })
+
+
+const Task = mongoose.model("Task",{
+	
+	description:{
+		type: String,
+		trim: true,
+		required: true,
+
+
+	},
+	status:{
+		type:  Boolean,
+		default: false
+	}
 })
 
-me.save().then(()=> {
-	console.log(me)
-}).catch((error) =>{
-	console.log(error);
+const taskOne = new Task({
+	
+	description: "Dance with friends",
+	status: false
+	
+})
+
+
+taskOne.save().then(() => {
+	console.log(taskOne)
+}).catch((error) => {
+	console.log(error)
 })
