3ca7d4a8bf6382b99ac3e8abb06a5fab0bfa3c95
Esmaelhbr <esmael7@live.com>
2021-11-15 17:19:27 -0500

    clean up index file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 require('./db/mongoose');
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter =  require('./routers/user')
 const taskRouter =  require('./routers/task')
 
@@ -12,17 +10,6 @@ const port = process.env.Port || 3000;
 app.use(express.json());
 app.use(userRouter)
 app.use(taskRouter)
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(port,()=>{
 	console.log("server is up on port " + port);
