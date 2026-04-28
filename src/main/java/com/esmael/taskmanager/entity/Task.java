87c94c0fa9fcb3c3d43ddf6b4bca92755140cbd7
Esmael Andrade <esmael7@live.com>
2026-04-28 11:26:02 -0400

    Added User and Task entities with relationship mapping

---
+++ src/main/java/com/esmael/taskmanager/entity/Task.java
@@ -0,0 +1,89 @@
+package com.esmael.taskmanager.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "title")
+	private String title;
+	@Column(name = "description")
+	private String description;
+	@Column(name = "status")
+	private boolean completed = false;
+	
+	@ManyToOne
+	@JoinColumn(name = "use_id")
+	private User user;
+	
+	public Task() {
+		
+	}
+
+	public Task(String title, String description, boolean completed) {
+		
+		this.title = title;
+		this.description = description;
+		this.completed = completed;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+	
+	
+	
+
+}
