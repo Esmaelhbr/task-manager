5dcca1ef390b3315aef65c7da1d4e4198ccc9632
Esmael Andrade <esmael7@live.com>
2026-04-29 12:26:54 -0400

    Implemented Task CRUD operations with service and controller

---
+++ src/main/java/com/esmael/taskmanager/controller/TaskController.java
@@ -0,0 +1,63 @@
+package com.esmael.taskmanager.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.esmael.taskmanager.entity.Task;
+import com.esmael.taskmanager.service.TaskService;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+	
+	private final TaskService taskService;
+	
+	public TaskController(TaskService taskService) {
+		this.taskService = taskService;
+		
+	}
+	@PostMapping
+	public Task createTask(@RequestBody Task task) {
+		return taskService.createTask(task);
+	}
+	
+	@GetMapping
+	public List<Task> getAllTasks(){
+		return taskService.getAllTasks();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Task> getTaskById(@PathVariable Long id){
+		
+		return taskService.getTaskById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task){
+		try {
+			return ResponseEntity.ok(taskService.updateTask(id, task));
+			
+		}catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteTask(@PathVariable Long id){
+		taskService.deleteTask(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
